a69e388079c544540e5c71eee9dcd7c5f60600db
市川 真一 <suigun1000@gmail.com>
2016-08-16 17:30:19 +0900

    UEFI 解説書を考慮した修正
    (1) エラー表示改善
    (2) gnu-efi の修正箇所の明確化

--- src/efi_main.c
+++ src/efi_main.c
@@ -3,27 +3,81 @@
 
 #include <efi.h>
 #include <efilib.h>
+#include "efi_status.h"
 
 #define true 1
 #define false 0
 
 typedef UINT8 bool;
 
+static void init(EFI_HANDLE image_handle, EFI_SYSTEM_TABLE* system_table);
+static void read_key(void);
+static void reset_system(EFI_STATUS status);
+static void error_print(CHAR16* msg, EFI_STATUS* status);
+static bool is_setup_mode(UINT8 pk, UINT8 audit_mode, UINT8 deployed_mode, UINT8 setup_mode, UINT8 secure_boot);
+static bool is_user_mode(UINT8 pk, UINT8 audit_mode, UINT8 deployed_mode, UINT8 setup_mode);
+static bool is_audit_mode(UINT8 pk, UINT8 audit_mode, UINT8 deployed_mode, UINT8 setup_mode, UINT8 secure_boot);
+static bool is_deployed_mode(UINT8 pk, UINT8 audit_mode, UINT8 deployed_mode, UINT8 setup_mode);
+static EFI_STATUS get_PK(UINT8* pk);
+static EFI_STATUS get_variable(CHAR16* variable_name, UINT8* data);
+static void get_pk_and_mode_variables(void);
+
+EFI_STATUS efi_main(EFI_HANDLE image_handle, EFI_SYSTEM_TABLE* system_table)
+{
+    init(image_handle, system_table);
+
+    Print(L"When you press any key, the system will reboot.\n");
+    Print(L"\n");
+
+    get_pk_and_mode_variables();
+
+    reset_system(EFI_SUCCESS);
+
+    return EFI_SUCCESS;
+}
+
+static void init(EFI_HANDLE image_handle, EFI_SYSTEM_TABLE* system_table)
+{
+    InitializeLib(image_handle, system_table);
+
+    EFI_STATUS status = EFI_SUCCESS;
+
+    if ((NULL == ST->ConIn) || (EFI_SUCCESS != (status = ST->ConIn->Reset(ST->ConIn, 0)))){
+
+        error_print(L"Input device unavailable.\n", ST->ConIn ? &status : NULL);
+    }
+}
+
+static void read_key(void)
+{
+    if (ST->ConIn){
+
+        EFI_STATUS local_status = EFI_SUCCESS;
+
+        do{
+            EFI_INPUT_KEY key;
+
+            local_status = ST->ConIn->ReadKeyStroke(ST->ConIn, &key);
+
+        } while (EFI_SUCCESS != local_status);
+    }
+}
+
 static void reset_system(EFI_STATUS status)
 {
-    EFI_STATUS local_status = EFI_SUCCESS;
-
-    do{
-        EFI_INPUT_KEY key;
-        local_status = ST->ConIn->ReadKeyStroke(ST->ConIn, &key);
-    } while (EFI_SUCCESS != local_status);
+    read_key();
 
     RT->ResetSystem(EfiResetCold, status, 0, NULL);
 }
 
-static void error_print(CHAR16* msg)
+static void error_print(CHAR16* msg, EFI_STATUS* status)
 {
     Print(msg);
+
+    if (status){
+
+        Print(L"EFI_STATUS = %d, %s\n", *status, print_status_msg(*status));
+    }
 
     reset_system(EFI_SUCCESS);
 }
@@ -113,36 +167,51 @@ static EFI_STATUS get_variable(CHAR16* variable_name, UINT8* data)
     return status;
 }
 
-static void get_pk_and_mode_variables()
+static void get_pk_and_mode_variables(void)
 {
     UINT8 pk = 0;
-    if (EFI_SUCCESS != get_PK(&pk)){
 
-        error_print(L"get_PK() failed.\n");
+    EFI_STATUS status = get_PK(&pk);
+
+    if (EFI_ERROR(status)){
+
+        error_print(L"get_PK() failed.\n", &status);
     }
 
     UINT8 audit_mode = 0;
-    if (EFI_SUCCESS != get_variable(L"AuditMode", &audit_mode)){
 
-        error_print(L"get_variable(AuditMode) failed.\n");
+    status = get_variable(L"AuditMode", &audit_mode);
+
+    if (EFI_ERROR(status)){
+
+        error_print(L"get_variable(AuditMode) failed.\n", &status);
     }
 
     UINT8 deployed_mode = 0;
-    if (EFI_SUCCESS != get_variable(L"DeployedMode", &deployed_mode)){
 
-        error_print(L"get_variable(DeployedMode) failed.\n");
+    status = get_variable(L"DeployedMode", &deployed_mode);
+
+    if (EFI_ERROR(status)){
+
+        error_print(L"get_variable(DeployedMode) failed.\n", &status);
     }
 
     UINT8 setup_mode = 0;
-    if (EFI_SUCCESS != get_variable(L"SetupMode", &setup_mode)){
 
-        error_print(L"get_variable(SetupMode) failed.\n");
+    status = get_variable(L"SetupMode", &setup_mode);
+
+    if (EFI_ERROR(status)){
+
+        error_print(L"get_variable(SetupMode) failed.\n", &status);
     }
 
     UINT8 secure_boot = 0;
-    if (EFI_SUCCESS != get_variable(L"SecureBoot", &secure_boot)){
 
-        error_print(L"get_variable(SecureBoot) failed.\n");
+    status = get_variable(L"SecureBoot", &secure_boot);
+
+    if (EFI_ERROR(status)){
+
+        error_print(L"get_variable(SecureBoot) failed.\n", &status);
     }
 
     Print(
@@ -171,19 +240,5 @@ static void get_pk_and_mode_variables()
 
         Print(L"The system is unknown mode.\n");
     }
-}
-
-EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SystemTable)
-{
-    InitializeLib(ImageHandle, SystemTable);
-
-    Print(L"When you press any key, the system will reboot.\n");
-    Print(L"\n");
-
-    get_pk_and_mode_variables();
-
-    reset_system(EFI_SUCCESS);
-
-    return EFI_SUCCESS;
 }
 
